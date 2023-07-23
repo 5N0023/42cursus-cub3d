81d38c9258e6a4af2e6602f497dc73eff4975364
5N0023 <mlektaibim@gmail.com>
2023-07-23 03:53:00 +0100

    need to norm

--- test.c
+++ test.c
@@ -1,32 +1,52 @@
-
 #include "cub3D.h"
 
+void *mlx_ptr;
+int posx = 50;
+int posy = 50;
+mlx_image_t *img = NULL;
+mlx_image_t *img2 = NULL;
 
 
+double playerx = 2;
+double playery = 2;
+double playerangel = 0;
+double hitx = 0;
+double hity = 0;
+int **map;
+double xray = -1;
+double yray = -1;
 int hit = 0;
+double hhitx = 0;
+double hhity = 0;
+double vhitx = 0;
+double vhity = 0;
+double equal = WINDOWW / 2;
 
-void draw_map(int x, int y, int size, int color, t_data *data)
+
+
+
+void draw_map(int **map, int x, int y, int size, int color)
 {
     for(int i = 0; i < x; i++)
         for(int j = 0; j < y; j++)
-            if(data->map->map[i][j] == '1')
+            if(map[i][j] == 1)
                 for(int k = 0; k < size; k++)
                     for(int l = 0; l < size; l++)
-                        mlx_put_pixel(data->img, i * size + k, j * size + l, color);
-            else if (data->map->map[i][j] == '0')
+                        mlx_put_pixel(img, i * size + k, j * size + l, color);
+            else if (map[i][j] == 0)
                 for(int k = 0; k < size; k++)
                     for(int l = 0; l < size; l++)
-                        mlx_put_pixel(data->img, i * size + k, j * size + l, 0x87CEEB0F);
-    x = data->player->x * WINDOWW / 50 - 3;
-    y = data->player->y * WINDOWW / 50 - 3;
+                        mlx_put_pixel(img, i * size + k, j * size + l, 0x87CEEB0F);
+    x = playerx * WINDOWW / 50 - 3;
+    y = playery * WINDOWW / 50 - 3;
     for(int i = 0; i < 6; i++)
         for(int j = 0; j < 6; j++)
-            mlx_put_pixel(data->img, x + i, y + j, 0xFF5F1FF);
+            mlx_put_pixel(img, x + i, y + j, 0xFF5F1FF);
     
 }
 
 
-void draw_line(int x0, int y0, int x1, int y1,int color,t_data *data) {
+void draw_line(int x0, int y0, int x1, int y1,int color) {
     
     if (y0 < 0)
         y0 = 0;
@@ -49,13 +69,13 @@ void draw_line(int x0, int y0, int x1, int y1,int color,t_data *data) {
             break;
         
         if (k == 0 && x0 != 0 && y0 != WINDOWW -1)
-            mlx_put_pixel(data->img, x0, y0, 0x000000FF);
+            mlx_put_pixel(img, x0, y0, 0x000000FF);
         else
-            mlx_put_pixel(data->img, x0, y0, color);
+            mlx_put_pixel(img, x0, y0, color);
         if (x0 == x1 && y0 == y1)
         {
             if (x0 != 0 && y0 != WINDOWW -1)
-                mlx_put_pixel(data->img, x0, y0, 0x000000FF);
+                mlx_put_pixel(img, x0, y0, 0x000000FF);
             break;
         }
         int e2 = 2 * err;
@@ -73,146 +93,162 @@ void draw_line(int x0, int y0, int x1, int y1,int color,t_data *data) {
 
 
 
-void  castrayvertical(t_ray *vray, t_data *data) {
-    double radian;
-
-    if (vray->angle <= 90 && vray->angle >= 0)
-        vray->dx = (int)vray->x + 1;
-    else if (vray->angle >= 0 && vray->angle <= 180)
-        vray->dx = (int)vray->x + 1;
-    else if (vray->angle >= 180 && vray->angle <= 360)
+void  castrayvertical(double x, double y, double angle, int **map) {
+    double dx, dy, radian;
+    double pa = angle;
+    
+    if(pa > 360)
+        pa -= 360;
+    if (pa < 0)
+        pa += 360;
+    if (pa <= 90 && pa >= 0)
+        dx = (int)x + 1;
+    else if (pa >= 0 && pa <= 180)
+        dx = (int)x + 1;
+    else if (pa >= 180 && pa <= 360)
         {
-            if (vray->dx == (int)vray->x)
-                vray->dx = (int)vray->x - 1;
+            if (dx == (int)x)
+                dx = (int)x - 1;
             else
-                vray->dx = (int)vray->x;
+                dx = (int)x;
         }
-    radian = vray->angle * M_PI / 180.0;
-    if (vray->angle != 0 && vray->angle != 180 && vray->angle != 360)
-        vray->dy = vray->y + (vray->dx - vray->x) / tanf(radian);
-    else if (vray->angle == 0 || vray->angle == 180 || vray->angle == 360)
+    radian = pa * M_PI / 180.0;
+    if (pa != 0 && pa != 180 && pa != 360)
+        dy = y + (dx - x) / tanf(radian);
+    else if (pa == 0 || pa == 180 || pa == 360)
         {
-            vray->dx = vray->x;
-            if (vray->angle == 0 || vray->angle == 360)
-                vray->dy = (int)vray->y + 1;
+            dx = x;
+            if (pa == 0 || pa == 360)
+                dy = (int)y + 1;
             else
                 {
-                    if (vray->dy != (int)vray->y)
-                        vray->dy = (int)vray->y;
+                    if (dy != (int)y)
+                        dy = (int)y;
                     else
-                        vray->dy = (int)vray->y - 1;
+                        dy = (int)y - 1;
                 }
         }
-    vray->x = vray->dx;
-    vray->y = vray->dy;
-    if(vray->x  <= 0 || vray->x >= 10 || vray->y <= 0 || vray->y >= 10)
+    xray = dx;
+    yray = dy;
+    if(xray < 0 || xray > 10 || yray < 0 || yray > 10)
     {
         hit = 1;
-        vray->x = vray->dx;
-        vray->y = vray->dy;
+        xray = dx;
+        yray = dy;
         return;
     }
-    if((vray->angle >= 180 && vray->angle <= 360) || vray->angle == 0)
+    if(pa >= 180 || pa <= 90)
         {
-            if (data->map->map[(int)vray->dx - 1][(int)vray->dy] == '1')
+            if (map[(int)dx - 1][(int)dy] == 1)
                 hit = 1;
         }
-    if (data->map->map[(int)vray->dx][(int)vray->dy] == '1')
+    if (map[(int)dx][(int)dy] == 1)
         hit = 1;
     
 }
 
-void castrayhorizontal(t_ray *hray, t_data *data) {
-    double radian;
+void castrayhorizontal(double x, double y, double angle, int **map) {
+    double dx, dy, radian;
+    double pa = angle;
 
-    radian = hray->angle * M_PI / 180.0;
+    if (pa >= 360)
+        pa -= 360;
+    else if (pa < 0)
+        pa += 360;
 
-    if (hray->angle >= 0 && hray->angle < 90)
-        hray->dy = (int)hray->y+1;
-    else if (hray->angle > 90 && hray->angle < 270)
+    radian = pa * M_PI / 180.0;
+
+    if (pa >= 0 && pa < 90)
+        dy = (int)y+1;
+    else if (pa > 90 && pa < 270)
         {
-            if (hray->dy == (int)hray->y)
-                hray->dy = (int)hray->y - 1;
+            if (dy == (int)y)
+                dy = (int)y - 1;
             else
-                hray->dy = (int)hray->y;
+                dy = (int)y;
         }
     else
-        hray->dy = (int)hray->y+1;
+        dy = (int)y+1;
 
-    if (hray->angle != 90 && hray->angle != 270) {
-        hray->dx = hray->x + (hray->dy - hray->y) * tanf(radian);
+    if (pa != 90 && pa != 270) {
+        dx = x + (dy - y) * tanf(radian);
     }
     else {
-        hray->dy = hray->y;
-        if (hray->angle == 90)
-            hray->dx = (int)hray->x + 1;
+        dy = y;
+        if (pa == 90)
+            dx = (int)x + 1;
         else
-            if (hray->dx != (int)(hray->x))
-                hray->dx = (int)hray->x;
+            if (dx != (int)(x))
+                dx = (int)x;
             else
             {
-                if(hray->dx == (int)hray->x)
-                    hray->dx = (int)hray->x - 1;
+                if(dx == (int)x)
+                    dx = (int)x - 1;
                 else
-                    hray->dx = (int)hray->x;
+                    dx = (int)x;
             }
                
     }
-    hray->x = hray->dx;
-    hray->y = hray->dy;
-    if(hray->x  < 0 || hray->x  > 10 || hray->y < 0 || hray->y > 10)
+    xray = dx;
+    yray = dy;
+    if(xray < 0 || xray > 10 || yray < 0 || yray > 10)
     {
         hit = 1;
-        hray->x = hray->dx;
-        hray->y = hray->dy;
+        xray = dx;
+        yray = dy;
         return;
     }
-    if(hray->angle >= 90 && hray->angle <= 270)
+    if(pa >= 90 && pa <= 270)
         {
-            if (data->map->map[(int)hray->dx][(int)hray->dy - 1] == '1')
+            if (map[(int)dx][(int)dy - 1] == 1)
                 hit = 1;
         }
-    if (data->map->map[(int)hray->dx][(int)hray->dy] == '1')
+    if (map[(int)dx][(int)dy] == 1)
         hit = 1;
 }
 
-double hits(double angle,t_data *data)
+double hits(double angle)
 {
+    xray = playerx;
+    yray = playery;
     if (angle >= 360)
         angle -= 360;
     else if (angle < 0)
         angle += 360;
-    t_ray hray;
-    t_ray vray;
-    data->ray->angle = angle;
-    hray.angle = angle;
-    hray.x = data->player->x;
-    hray.y = data->player->y;
+    double vhitx = WINDOWW;
+    double vhity = WINDOWW;
+    double hhitx = WINDOWW;
+    double hhity = WINDOWW;
+    xray = playerx;
+    yray = playery;
     hit = 0;
     while(!hit)
-        castrayvertical(&vray,data);
+        castrayvertical(xray, yray, angle, map);
+    vhitx = xray;
+    vhity = yray;
+    xray = playerx;
+    yray = playery;
     hit = 0;
-    vray.angle = angle;
-    vray.x = data->player->x;
-    vray.y = data->player->y;
     while(!hit)
-        castrayhorizontal(&hray, data);
-    vray.distance = sqrtf(powf((vray.dx - data->player->x), 2) + powf((vray.dy - data->player->y), 2));
-    hray.distance = sqrtf(powf((hray.dx - data->player->x), 2) + powf((hray.dy - data->player->y), 2));
-    if (vray.distance < hray.distance)
+        castrayhorizontal(xray, yray, angle, map);
+    hhitx = xray;
+    hhity = yray;
+    double vdistance = sqrtf(powf((vhitx - playerx), 2) + powf((vhity - playery), 2));
+    double hdistance = sqrtf(powf((hhitx - playerx), 2) + powf((hhity - playery), 2));
+    double distance;
+    if (hdistance < vdistance)
     {
-        data->ray->x = vray.dx;
-        data->ray->y = vray.dy;
-        data->ray->distance = vray.distance;
-
+        hitx = hhitx;
+        hity = hhity;
+        distance = hdistance;
     }
     else
     {
-        data->ray->x = hray.dx;
-        data->ray->y = hray.dy;
-        data->ray->distance = hray.distance;
+        hitx = vhitx;
+        hity = vhity;
+        distance = vdistance;
     }
-    return (data->ray->distance);
+    return(distance);
 }
 
 
@@ -221,179 +257,170 @@ double hits(double angle,t_data *data)
 
 
 
-mlx_keyfunc key_hook(mlx_key_data_t key, t_data *data)
+mlx_keyfunc key_hook(mlx_key_data_t key, void *param)
 {
-    double x = data->player->x;
-    double y = data->player->y;
+    double x = playerx;
+    double y = playery;
     double distance1;
     double distance2;
     if (key.key == MLX_KEY_ESCAPE)
         exit(0);
     if(key.key == MLX_KEY_RIGHT)
-        data->player->angle -= data->player->rotation;
+        playerangel -= 2;
     if(key.key == MLX_KEY_LEFT)
-        data->player->angle += data->player->rotation;
+        playerangel += 2;
     if(key.key == MLX_KEY_S)
     {
-        distance1 = hits(data->player->angle+10+180,data);
-        distance2 = hits(data->player->angle-10+180,data);
+        distance1 = hits(playerangel+10+180);
+        distance2 = hits(playerangel-10+180);
         if (distance1 > 0.2 && distance2 > 0.2)
         {
-            x += cosf((data->player->angle+90) * M_PI / 180.0) * data->player->speed;
-            y -= sinf((data->player->angle+90) * M_PI / 180.0) * data->player->speed;
+            x += cosf((playerangel+90) * M_PI / 180.0) * 0.15;
+            y -= sinf((playerangel+90) * M_PI / 180.0) * 0.15;
         }
+        printf("distance1 %f distance2 %f \n",distance1,distance2);
     }
     if(key.key == MLX_KEY_W)
     {
-        distance1 = hits(data->player->angle+10,data);
-        distance2 = hits(data->player->angle-10,data);
+        distance1 = hits(playerangel+10);
+        distance2 = hits(playerangel-10);
         if (distance1 > 0.2 && distance2 > 0.2)
         {
-            x -= cosf((data->player->angle+90) * M_PI / 180.0) * data->player->speed;
-            y += sinf((data->player->angle+90) * M_PI / 180.0) * data->player->speed;
+            x -= cosf((playerangel+90) * M_PI / 180.0) * 0.15;
+            y += sinf((playerangel+90) * M_PI / 180.0) * 0.15;
         }
     }
     if(key.key == MLX_KEY_D)
     {
-        distance1 = hits(data->player->angle+10-90,data);
-        distance2 = hits(data->player->angle-10-90,data);
+        distance1 = hits(playerangel+10-90);
+        distance2 = hits(playerangel-10-90);
         if (distance1 > 0.2 && distance2 > 0.2)
         {
-        x -= cosf((data->player->angle) * M_PI / 180.0) * data->player->speed;
-        y += sinf((data->player->angle) * M_PI / 180.0) * data->player->speed;
+        x -= cosf((playerangel) * M_PI / 180.0) * 0.15;
+        y += sinf((playerangel) * M_PI / 180.0) * 0.15;
         }
     }
     if(key.key == MLX_KEY_A)
     {
-        distance1 = hits(data->player->angle+10+90,data);
-        distance2 = hits(data->player->angle-10+90,data);
+        distance1 = hits(playerangel+10+90);
+        distance2 = hits(playerangel-10+90);
         if (distance1 > 0.2 && distance2 > 0.2)
         {
-        x += cosf((data->player->angle) * M_PI / 180.0) * data->player->speed;
-        y -= sinf((data->player->angle) * M_PI / 180.0) * data->player->speed;
+        x += cosf((playerangel) * M_PI / 180.0) * 0.15;
+        y -= sinf((playerangel) * M_PI / 180.0) * 0.15;
         }
     }
-    data->player->x = x;
-    data->player->y = y;
+    playerx = x;
+    playery = y;
     return (NULL);
 }
 
-mlx_mousefunc key_mouse(mouse_key_t button,action_t action, modifier_key_t mods,t_data *data)
+mlx_mousefunc key_mouse(mouse_key_t button,action_t action, modifier_key_t modifier_key,void *param)
 {
-    
+    if(button == MLX_MOUSE_BUTTON_LEFT && action == MLX_PRESS)
+        printf("left click at %d %d\n",posx,posy);
+    if(button == MLX_MOUSE_BUTTON_RIGHT && action == MLX_PRESS)
+        printf("right click at %d %d\n",posx,posy);
+    if(button == MLX_MOUSE_BUTTON_MIDDLE && action == MLX_PRESS)
+        printf("middle click at %d %d\n",posx,posy);
+    if(button == MLX_MOUSE_BUTTON_LEFT && action == MLX_RELEASE)
+        printf("left release at %d %d\n",posx,posy);
+    if(button == MLX_MOUSE_BUTTON_RIGHT && action == MLX_RELEASE)
+        printf("right release at %d %d\n",posx,posy);
+    if(button == MLX_MOUSE_BUTTON_MIDDLE && action == MLX_RELEASE)
+        printf("middle release at %d %d\n",posx,posy);
+    printf("button %d action %d modifier %d\n",button,action,modifier_key);
     return (NULL);
     
 }
-mlx_cursorfunc key_cursor(double x,double y,t_data *data)
+mlx_cursorfunc key_cursor(double x,double y,void *param)
 {
     static double oldx;
     static double oldy;
     if (oldx < x)
-        data->player->angle -= 5;
+        playerangel -= 3;
     else
-        data->player->angle += 5;
+        playerangel += 3;
     if (oldy < y)
-        {if (data->center - 5 > 0)
-            data->center -= 5;}
+        {if (equal - 5 > 0)
+            equal -= 5;}
     else
-        {if (data->center + 5 < WINDOWW)
-            data->center += 5;}
+        {if (equal + 5 < WINDOWW )
+            equal += 5;}
     oldx = x;
     oldy = y;
     return (NULL);
 }
 
 
-int my_mlx_loop_hook(t_data *data)
+int my_mlx_loop_hook(void *param)
 {
 
-    if(data->img != NULL)
-        mlx_delete_image(data->mlx,data->img);
-    data->img = mlx_new_image(data->mlx, WINDOWW, WINDOWW);
+    if(img != NULL)
+        mlx_delete_image(mlx_ptr,img);
+    double POV = 60;
+    img = mlx_new_image(mlx_ptr, WINDOWW, WINDOWW);
+    draw_map(map, 10, 10, WINDOWW / 50, 0x00FFFF);
     for(int i = 0; i < WINDOWW; i++)
-        for(int j = data->center; j < WINDOWW; j++)
-            mlx_put_pixel(data->img, i, j, 0x362624FF);
+        for(int j = equal; j < WINDOWW; j++)
+            mlx_put_pixel(img, i, j, 0x00FFFF);
     for(int i = 0; i < WINDOWW; i++)
-        for(int j = 0; j < data->center; j++)
-            mlx_put_pixel(data->img, i, j, 0x87CEEBFF);
-    if(data->player->angle >= 360)
-        data->player->angle -= 360;
-    else if(data->player->angle < 0)
-        data->player->angle += 360;
-    double angle = data->player->angle - FOV / 2;
+        for(int j = 0; j < equal; j++)
+            mlx_put_pixel(img, i, j, 0x87CEEBFF);
+    if(playerangel >= 360)
+        playerangel -= 360;
+    else if(playerangel < 0)
+        playerangel += 360;
+    double angle = playerangel - POV / 2;
     if (angle < 0)
         angle += 360;
     if (angle >= 360)
         angle -= 360;
     double draw = 0;
+    int k = WINDOWW -1;
     
-    int k = WINDOWW - 1;
-    while (k >= 0 )
+    while (k >= 0)
     {
-        
         double a = angle;
-        double distance = hits(a,data) * cosf((a - data->player->angle) * M_PI / 180.0);
-        
-        if (distance == 0)
-            distance = 0.001;
-        double wallheight = (WINDOWW / distance) - 1;
+        double distance = hits(a) * cosf((a - playerangel) * M_PI / 180.0);
+        double wallheight = WINDOWW / distance;
         int color = 0x964B00FF;
         color = color - (int)(distance * 10);
-        draw_line(k, data->center - wallheight / 2, k, data->center + wallheight / 2, color,data );
-        angle += data->fov / WINDOWW;
-        draw += data->fov  / WINDOWW;
+        draw_line(k, equal - wallheight / 2, k, equal + wallheight / 2, color );
+        angle += POV / WINDOWW;
+        draw += POV / WINDOWW;
         k--;
     }
-    draw_map(10, 10, WINDOWW / 50, 0x00FF0F,data);
-    mlx_image_to_window(data->mlx,data->img, 0, 0);
+    draw_map(map, 10, 10, WINDOWW / 50, 0x00FF0F);
+    mlx_image_to_window(mlx_ptr, img, 0, 0);
     return (0);
 }
 
 
 int main()
 {
-    t_data data;
-    t_map map;
-    t_player player;
-    t_data *data_ptr = &data;
-    t_ray ray;
-    data_ptr->ray = &ray;
-    data_ptr->map = &map;
-    data_ptr->player = &player;
-
-    data_ptr->map->width = 10;
-    data_ptr->map->height = 10;
-    data_ptr->map->map = (char **)malloc(10 * sizeof(char *));
+    map = (int **)malloc(10 * sizeof(int *));
     for (int i = 0; i < 10; i++) {
-        data_ptr->map->map[i] = (char *)malloc(10 * sizeof(char));
+        map[i] = (int *)malloc(10 * sizeof(int));
     }
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            data_ptr->map->map[i][j] = '0';
+            map[i][j] = 0;
             if (i == 0 || i == 9 || j == 0 || j == 9) {
-                data_ptr->map->map[i][j] = '1';
+                map[i][j] = 1;
             }
         }
     }
-    data_ptr->map->map[4][5] = '1';
-    data_ptr->map->map[5][4] = '1';
-    data_ptr->map->map[7][3] = '1';
-    data_ptr->mlx = NULL;
-    data_ptr->img = NULL;
-    data_ptr->player->angle = 0;
-    data_ptr->player->x = 2;
-    data_ptr->player->y = 2;
-    data_ptr->player->speed = 0.1;
-    data_ptr->player->rotation = 2.5;
-    data_ptr->center = WINDOWW / 2;
-    data_ptr->fov = FOV;
-    data_ptr->mlx = mlx_init(WINDOWW, WINDOWW, "CUB3D!",1);
-    mlx_loop_hook(data_ptr->mlx, (void *)my_mlx_loop_hook, data_ptr);
-    mlx_key_hook(data_ptr->mlx, (void *)key_hook, data_ptr);
-    mlx_mouse_hook(data_ptr->mlx, (void *)key_mouse, data_ptr);
-    mlx_set_cursor_mode(data_ptr->mlx, MLX_MOUSE_DISABLED);
-    mlx_cursor_hook(data_ptr->mlx, (void *)key_cursor, data_ptr);
-    mlx_loop(data_ptr->mlx);
+    map[4][5] = 1;
+    map[5][4] = 1;
+    img = NULL;
+    mlx_ptr = mlx_init(WINDOWW, WINDOWW, "CUB3D!",1);
+    mlx_loop_hook(mlx_ptr, (void *)my_mlx_loop_hook, NULL);
+    mlx_key_hook(mlx_ptr, (void *)key_hook, NULL);
+    mlx_mouse_hook(mlx_ptr, (void *)key_mouse, NULL);
+    mlx_set_cursor_mode(mlx_ptr, MLX_MOUSE_DISABLED);
+    mlx_cursor_hook(mlx_ptr, (void *)key_cursor, NULL);
+    mlx_loop(mlx_ptr);
     return (0);
 }
 
